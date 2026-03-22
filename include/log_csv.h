1aeeee789cb1d6ed9d9749a5c3a451cf8dbaae31
Bernd Klein <berndklein@gmx.de>
2026-03-22 08:28:42 +0100

    T15 - Abschlußstand

--- include/log_csv.h
+++ include/log_csv.h
@@ -13,12 +13,20 @@
 // ---------------------------------------------------------------
 
 namespace csv {
-
+// Temperature output, millivolts, ohms, tempearture for hotSpot & Chamber, some state values
 struct TEMP {
     static constexpr const char *PREFIX = "TEMP";
     // [CSV_<PREFIX>];rawHot;hotMilliVolts;tempHot_dC;rawChamber;chamberMilliVolts;tempChamber_dC;heater_on;door_open;state
     static constexpr const char *FMT =
         "[CSV_%s];%ld;%ld;%ld;%ld;%ld;%ld;%d;%d;%d\n";
+};
+
+// State output, door_open, heater, fan12V, fan230V, fan230V_slow, motor
+struct STATE {
+    static constexpr const char *PREFIX = "STATE";
+    // [CSV_<PREFIX>]; \n
+    static constexpr const char *FMT =
+        "[CSV_%s];%d;%d;%d;%d;%d;%d\n";
 };
 
 } // namespace csv
@@ -38,6 +46,17 @@ struct TEMP {
 #define CSV_LOG_TEMP(...)                                          \
     do {                                                           \
         CSV_LOG(csv::TEMP::PREFIX, csv::TEMP::FMT, ##__VA_ARGS__); \
+    } while (0)
+#else
+#define CSV_LOG_TEMP(...) \
+    do {                  \
+    } while (0)
+#endif
+
+#ifdef CSV_OUT
+#define CSV_LOG_STATE(...)                                          \
+    do {                                                           \
+        CSV_LOG(csv::TEMP::PREFIX, csv::STATE::FMT, ##__VA_ARGS__); \
     } while (0)
 #else
 #define CSV_LOG_TEMP(...) \
